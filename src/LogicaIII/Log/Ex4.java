b816053c0b5676b001581a1641853c80b6774924
Dev Mello <robertale45@gmail.com>
2025-03-24 15:54:27 -0300

    refatorando

--- src/LogicaIII/Log/Ex4.java
+++ src/LogicaIII/Log/Ex4.java
@@ -1,10 +1,12 @@
 package LogicaIII.Log;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ex4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
         String vendedor;
         double salarioFixo,totalVendas,comissao, salarioFinal;
         System.out.println("Qual nome do vendedor:");
@@ -16,7 +18,7 @@ public class Ex4 {
 
         comissao = totalVendas * 0.15;
         salarioFinal = salarioFixo + comissao;
-        System.out.println(vendedor);
+        System.out.println("\nResumo do vendedor: "+vendedor);
         System.out.printf("Salario Fixo do vendedor é : %.2f%n" ,salarioFixo);
         System.out.printf("Total de vendas foi de : %.2f%n",totalVendas);
         System.out.printf("Sua comissão foi de : %.2f%n",comissao);
