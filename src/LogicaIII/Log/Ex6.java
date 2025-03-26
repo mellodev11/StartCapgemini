5948c8b2627aaabf07c167b1c68053e76e0e9f78
Dev Mello <robertale45@gmail.com>
2025-03-26 14:15:48 -0300

    Comentários

--- src/LogicaIII/Log/Ex6.java
+++ src/LogicaIII/Log/Ex6.java
@@ -2,6 +2,9 @@ package LogicaIII.Log;
 
 import java.util.Locale;
 import java.util.Scanner;
+//Leia dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variávelApasse a
+//possuir ovalor da variável B e a variável B passe a possuir o valor da variável A.Apresentar osvalores
+//trocados;
 
 public class Ex6 {
     public static void main(String[] args) {
